80adefb9b1e20d09675abcd6ced39ee54fe30ef2
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-07-11 23:14:29 +0200

    new(github/repo)

---
+++ github/repo/repo.tf
@@ -0,0 +1,81 @@
+variable "name" {
+  type = string
+}
+variable "description" {
+  type = string
+}
+variable "public" {
+  type = bool
+}
+variable "license" {
+  type    = string
+  default = "mit"
+}
+variable "pullers" {
+  type    = list(string)
+  default = []
+}
+variable "pushers" {
+  type    = list(string)
+  default = []
+}
+variable "maintainers" {
+  type    = list(string)
+  default = []
+}
+variable "triagers" {
+  type    = list(string)
+  default = []
+}
+variable "admins" {
+  type    = list(string)
+  default = []
+}
+
+resource "github_repository" "main" {
+  name                 = var.name
+  description          = var.description
+  visibility           = var.public ? "public" : "private"
+  auto_init            = true
+  vulnerability_alerts = true
+  license_template     = var.license
+}
+
+resource "github_repository_collaborators" "some_repo_collaborators" {
+  repository = github_repository.main.name
+  dynamic "user" {
+    for_each = toset(var.pullers)
+    content {
+      permission = "pull"
+      username   = each.key
+    }
+  }
+  dynamic "user" {
+    for_each = toset(var.pushers)
+    content {
+      permission = "push"
+      username   = each.key
+    }
+  }
+  dynamic "user" {
+    for_each = toset(var.maintainers)
+    content {
+      permission = "maintain"
+      username   = each.key
+    }
+  }
+  dynamic "user" {
+    for_each = toset(var.triagers)
+    content {
+      permission = "triage"
+      username   = each.key
+    }
+  }
+  dynamic "user" {
+    for_each = toset(var.admins)
+    content {
+      permission = "admin"
+      username   = each.key
+    }
+  }
+}
